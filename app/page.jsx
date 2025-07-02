1c23e24bed7f77fdeae2a4c9d49f9a0c74e9fc61
SharwanKunwar <sharwankunwar07@gmail.com>
2025-07-02 22:07:26 +0545

    updated

--- app/page.jsx
+++ app/page.jsx
@@ -4,6 +4,9 @@ import { Container } from "./components/Container";
 import Projects from "./components/Projects";
 import { useState } from "react";
 import {motion} from 'motion/react';
+import { FaFacebook,FaLinkedin,FaGithub } from 'react-icons/fa';
+
+
 
 
 export default function Home() {
@@ -66,8 +69,32 @@ export default function Home() {
           className="mt-3"
           >
           <p className="text-neutral-400 dark:text-neutral-100 pt-3 md:text-sm text-sm max-w-2xl">I specialize in building scalable, efficient systems and crafting digital experiences that don’t just work  they wow. I’m currently building my own path (yes, solo dev life ✌️), working on projects that combine logic, aesthetics, and a whole lot of coffee.</p>
-          <p className="text-neutral-400 dark:text-neutral-100 pt-3 md:text-sm text-sm max-w-2xl">I speak fluent Java, C, JavaScript, and sarcasm, and I have a not-so-secret obsession with optimizing everything — from backend performance to how my terminal looks.</p>
-          <p id="projects" className="text-neutral-400 dark:text-neutral-100 pt-3 md:text-sm text-sm max-w-2xl">Whether it's building a full-stack app, architecting clean APIs, or animating a “404 page not found” to dance — I’m here for it.</p>
+          <p className="text-neutral-400 dark:text-neutral-100 pt-3 md:text-sm text-sm max-w-2xl">I speak fluent <strong>Java</strong>, <strong>C</strong>, <strong>JavaScript</strong>, and sarcasm, and I have a not-so-secret obsession with optimizing everything — from backend performance to how my terminal looks.</p>
+          <p id="projects" className="text-neutral-400 dark:text-neutral-100 pt-3 md:text-sm text-sm max-w-2xl">Whether it's building a full-stack app, architecting clean APIs, or animating a <strong>“404 page not found”</strong> to dance — I’m here for it.</p>
+          <section className="mt-10 w-full text-neutral-400 dark:text-neutral-100 md:text-sm text-sm max-w-2xl">
+            <p>If Googling me is your thing—I'm flattered. You can find me across the web here:</p>
+            <div className="grid md:grid-cols-10 grid-cols-7 gap-2 md:mt-3 mt-5">
+              <section className=" flex justify-start items-center">
+                <a
+                  href="https://www.facebook.com/sravana.kumvara/" target="_blank" rel="noopener noreferrer" aria-label="Facebook">
+                  <FaFacebook className="text-blue-400 w-6 h-6 hover:text-blue-200 transition-colors duration-200" />
+                </a>
+              </section>
+              <section className=" flex justify-start items-center ">
+                <a
+                  href="https://www.linkedin.com/in/sharwan-kunwar-95a919317/" target="_blank" rel="noopener noreferrer" aria-label="LinkedIn" >
+                  <FaLinkedin className="text-blue-400 w-6 h-6 hover:text-blue-200 transition-colors duration-200" />
+                </a>
+              </section>
+              <section className="flex justify-start items-center ">
+                <a
+                  href="https://github.com/SharwanKunwar" target="_blank" rel="noopener noreferrer" aria-label="GitHub">
+                  <FaGithub className="text-black w-6 h-6 hover:text-gray-300 transition-colors duration-200" />
+                </a>
+              </section>
+            </div>
+          </section>
+
         </motion.section>
         
         )}
